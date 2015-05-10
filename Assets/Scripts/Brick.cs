5f6528e551ad7cdee639192f02bb31ddf63aa69c
Piri-Piri <david.pirih@gmx.de>
2015-05-10 02:43:53 +0200

    first commit

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+	private LevelManager levelManager;
+	public int maxHits;
+	private int timesHits;
+	
+	// Use this for initialization
+	void Start () {
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+		timesHits = 0;
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+		timesHits++;
+		if (timesHits >= maxHits) { Destroy(gameObject); }
+	}
+	
+	// TODO Remove this method once we can actually win!
+	
+	void SimulateWin () {
+		levelManager.LoadNextLevel();
+	}
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		//print("Ball Bounces (Collision)");
+	}
+	
+	void Update () {
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
+	
+		// get mouse Position in world unit
+		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+		
+		// adjust x to mouse position, but add constraints to paddle
+		paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
+		
+		this.transform.position =  paddlePos;
+	}
+}
